62cd183a15e1365908e344617e4a0daac9e4ce58
hakanaydogmus2 <haydogmus14@outlook.com>
2024-04-28 18:05:08 +0300

    pull

--- Repositories/HybridAnalysisRepository/HybridLinkRepository.cs
+++ Repositories/HybridAnalysisRepository/HybridLinkRepository.cs
@@ -3,11 +3,13 @@ using RestSharp.Authenticators;
 using securityApp.Helper;
 using securityApp.Interfaces.IHybridAnalysesRepository;
 using Newtonsoft.Json;
+using Microsoft.AspNetCore.Mvc;
 
 namespace securityApp.Repositories.HybridAnalysesRepository
 {
     public class HybridLinkRepository : IHybridLinkRepository
     {
+        
         private readonly HybridAnalysisSettings _hybridAnalysisSettings;
         public HybridLinkRepository(HybridAnalysisSettings hybridAnalysisSettings)
         {
@@ -29,30 +31,6 @@ namespace securityApp.Repositories.HybridAnalysesRepository
 
         public async Task<RestResponse> PostUrlAsync(string url)
         {
-            Console.WriteLine($"{_hybridAnalysisSettings.QuickScanUrl}");
-            var options = new RestClientOptions(_hybridAnalysisSettings.QuickScanUrl);
-            var client = new RestClient(options);
-            var request = new RestRequest("");
-            request.AddHeader("accept", "application/json");
-            request.AddHeader("api-key", _hybridAnalysisSettings.ApiKey);
-            request.AddHeader("content-Type", "application/x-www-form-urlencoded");
-            Console.WriteLine(url);
-            request.AddParameter("scan_type", "all");
-            request.AddParameter("url", url);
-            request.AddParameter("comment", "");
-            request.AddParameter("submit_name", "");
-            Console.WriteLine(request.ToString());
-            try
-            {
-                var response = await client.PostAsync(request);
-                return response;
-            }
-            catch(Exception ex)
-            {
-                Console.WriteLine(ex.ToString());
-                return null;
-            }
-            
             return null;
         }
     }
